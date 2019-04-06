3a8a5f80030a3ca3fe5b4e346895bfd449f422ae
Pawel Psztyc <jarrodek@gmail.com>
2019-04-05 18:48:46 -0700

    Update: Updaiting import paths to match web spec

--- arc-data-import.js
+++ arc-data-import.js
@@ -11,7 +11,7 @@ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 License for the specific language governing permissions and limitations under
 the License.
 */
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
+import {PolymerElement} from '../../@polymer/polymer/polymer-element.js';
 import './import-data-store.js';
 import {ArcLegacyTransformer} from './transformers/arc-legacy-transformer.js';
 import {ArcDexieTransformer} from './transformers/arc-dexie-transformer.js';
